25876c7566eba2c34f7e08c293e572b660ee8930
Vignesh S <vignesh31.sk@gmail.com>
2024-12-01 20:21:09 +0530

    Added metamask connect

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -1,144 +1,203 @@
-"use client";  // Add this directive to indicate that this is a client component
+"use client"; // Add this directive to indicate that this is a client component
 
-import { useState, ChangeEvent, KeyboardEvent, useEffect } from 'react';
-import styles from '../../index.module.css';
+import { useState, ChangeEvent, KeyboardEvent, useEffect } from "react";
+import styles from "../../index.module.css";
+import Web3 from "web3";
 
 declare global {
-    interface Window {
-        SpeechRecognition: any;
-        webkitSpeechRecognition: any;
-    }
+  interface Window {
+    SpeechRecognition: any;
+    webkitSpeechRecognition: any;
+  }
 }
 
 const Home: React.FC = () => {
-    const [isDay, setIsDay] = useState<boolean>(true);
-    const [message, setMessage] = useState<string>('');
-    const [chatHistory, setChatHistory] = useState<string[]>([]);
-    const [recognition, setRecognition] = useState<SpeechRecognition | null>(null);
-    const [historyIndex, setHistoryIndex] = useState<number>(-1); // Track the index of the chat history
+  const [web3, setWeb3] = useState<Web3 | null>(null);
+  const [connectedAccount, setConnectedAccount] = useState<string>("");
+  const [nonce, setNonce] = useState<bigint | null>(null);
+  const [isDay, setIsDay] = useState<boolean>(true);
+  const [message, setMessage] = useState<string>("");
+  const [chatHistory, setChatHistory] = useState<string[]>([]);
+  const [recognition, setRecognition] = useState<SpeechRecognition | null>(
+    null
+  );
+  const [historyIndex, setHistoryIndex] = useState<number>(-1); // Track the index of the chat history
 
-    useEffect(() => {
-        const SpeechRecognition = window.SpeechRecognition || (window as any).webkitSpeechRecognition;
-        if (SpeechRecognition) {
-            const recognitionInstance = new SpeechRecognition();
-            recognitionInstance.continuous = false;
-            recognitionInstance.interimResults = false;
-            recognitionInstance.lang = 'en-US';
+  useEffect(() => {
+    async function init() {
+      // initialize web-3
+      if ((window as any).ethereum) {
+        const web3Instance = new Web3((window as any).ethereum);
+        setWeb3(web3Instance);
+        await (window as any).ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        const accounts = await web3Instance.eth.getAccounts();
+        setConnectedAccount(accounts[0]);
 
-            recognitionInstance.onresult = (event) => {
-                const transcript = event.results[0][0].transcript;
-                setMessage(transcript);
-            };
+        // Get the nonce
+        const nonceValue = await web3Instance.eth.getTransactionCount(
+          accounts[0]
+        );
+        setNonce(nonceValue);
+      } else {
+        alert("Please install MetaMask");
+      }
+    }
+    init();
 
-            recognitionInstance.onerror = (event) => {
-                console.error('Speech recognition error', event);
-            };
+    // initialize speech recognition
+    const SpeechRecognition =
+      window.SpeechRecognition || (window as any).webkitSpeechRecognition;
+    if (SpeechRecognition) {
+      const recognitionInstance = new SpeechRecognition();
+      recognitionInstance.continuous = false;
+      recognitionInstance.interimResults = false;
+      recognitionInstance.lang = "en-US";
 
-            setRecognition(recognitionInstance);
-        } else {
-            console.error('Speech Recognition not supported in this browser.');
-        }
-    }, []);
+      recognitionInstance.onresult = (event) => {
+        const transcript = event.results[0][0].transcript;
+        setMessage(transcript);
+      };
 
-    const toggleTheme = () => {
-        setIsDay(!isDay);
-    };
+      recognitionInstance.onerror = (event) => {
+        console.error("Speech recognition error", event);
+      };
 
-    const connectWallet = () => {
-        window.open("https://metamask.io/", "_blank");
-    };
+      setRecognition(recognitionInstance);
+    } else {
+      console.error("Speech Recognition not supported in this browser.");
+    }
+  }, []);
 
-    const handleSendMessage = () => {
-        if (message.trim()) {
-            setChatHistory([...chatHistory, message]);
-            setMessage('');
-            setHistoryIndex(-1);
-        }
-    };
+  const toggleTheme = () => {
+    setIsDay(!isDay);
+  };
 
-    const handleSendVoice = () => {
-        if (recognition) {
-            recognition.start();
-        }
-    };
+  const connectWallet = async () => {
+    if (web3 && (window as any).ethereum) {
+      try {
+        await (window as any).ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        const accounts = await web3.eth.getAccounts();
+        setConnectedAccount(accounts[0]);
 
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setMessage(e.target.value);
-    };
+        // Get the nonce
+        const nonceValue = await web3.eth.getTransactionCount(accounts[0]);
+        setNonce(nonceValue);
 
-    const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
-        if (e.key === 'Enter') {
-            handleSendMessage();
-        } else if (e.key === 'ArrowUp') {
-            if (historyIndex < chatHistory.length - 1) {
-                setHistoryIndex(historyIndex + 1);
-                setMessage(chatHistory[historyIndex + 1]);
-            }
-        } else if (e.key === 'ArrowDown') {
-            if (historyIndex > 0) {
-                setHistoryIndex(historyIndex - 1);
-                setMessage(chatHistory[historyIndex - 1]);
-            } else if (historyIndex === 0) {
-                setHistoryIndex(-1);
-                setMessage('');
-            }
-        }
-    };
+        console.log(`Wallet Address : ${connectedAccount}`);
+      } catch (error) {
+        console.error("Error connecting to MetaMask:", error);
+      }
+    } else {
+      alert("Please install MetaMask");
+    }
+  };
 
-    return (
-        <div className={`${styles.container} ${isDay ? styles.day : styles.night}`}>
-            <div className={styles.sidebar}>
-                <div className={styles.menu}>
-                    <ul>
-                        <li>Home</li>
-                        <li>Settings</li>
-                        <li>Profile</li>
-                        <li>Help</li>
-                    </ul>
-                </div>
-            </div>
-            <div className={styles.content}>
-                <header className={styles.header}>
-                    <h1>Zapp:) </h1>
-                    <div className={styles.controls}>
-                        <label className={styles.toggleSwitch}>
-                            <input type="checkbox" checked={isDay} onChange={toggleTheme} />
-                            <span className={styles.slider}></span>
-                        </label>
-                        <button onClick={connectWallet} style={{ width: '100px', height: '80px' }} >
-                        Connect Wallet
-                        </button>
-                    </div>
-                </header>
-                <div className={styles.chatBox}>
-                    <div className={styles.chatHistory}>
-                        {chatHistory.map((msg, index) => (
-                            <div key={index} className={styles.chatMessage}>
-                                {msg}
-                            </div>
-                        ))}
-                    </div>
-                    <div className={styles.inputContainer}>
-                        <input
-                            type="text"
-                            value={message}
-                            onChange={handleChange}
-                            onKeyDown={handleKeyDown}
-                            placeholder="Type a message..."
-                            className={styles.input}
-                        />
-                        <button onClick={handleSendMessage} className={styles.sendButton}>
-                            <img src="/pointer.png" alt="Arrow" style={{ width: '28px', height: '28px' }} />
-                        </button>
-                        <button onClick={handleSendVoice} className={styles.voiceButton}>
-                            <img src="/miccc.png" alt="Microphone" style={{ width: '26px', height: '26px' }} />
-                        </button>
-                        <button className={styles.newChatButton}>+</button>
-                    </div>
-                </div>
-            </div>
+  const handleSendMessage = () => {
+    if (message.trim()) {
+      setChatHistory([...chatHistory, message]);
+      setMessage("");
+      setHistoryIndex(-1);
+    }
+  };
+
+  const handleSendVoice = () => {
+    if (recognition) {
+      recognition.start();
+    }
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setMessage(e.target.value);
+  };
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleSendMessage();
+    } else if (e.key === "ArrowUp") {
+      if (historyIndex < chatHistory.length - 1) {
+        setHistoryIndex(historyIndex + 1);
+        setMessage(chatHistory[historyIndex + 1]);
+      }
+    } else if (e.key === "ArrowDown") {
+      if (historyIndex > 0) {
+        setHistoryIndex(historyIndex - 1);
+        setMessage(chatHistory[historyIndex - 1]);
+      } else if (historyIndex === 0) {
+        setHistoryIndex(-1);
+        setMessage("");
+      }
+    }
+  };
+
+  return (
+    <div className={`${styles.container} ${isDay ? styles.day : styles.night}`}>
+      <div className={styles.sidebar}>
+        <div className={styles.menu}>
+          <ul>
+            <li>Home</li>
+            <li>Settings</li>
+            <li>Profile</li>
+            <li>Help</li>
+          </ul>
         </div>
-    );
+      </div>
+      <div className={styles.content}>
+        <header className={styles.header}>
+          <h1>Zapp:) </h1>
+          <div className={styles.controls}>
+            <label className={styles.toggleSwitch}>
+              <input type="checkbox" checked={isDay} onChange={toggleTheme} />
+              <span className={styles.slider}></span>
+            </label>
+            <button
+              onClick={connectWallet}
+              style={{ width: "100px", height: "80px" }}
+            >
+              Connect Wallet
+            </button>
+          </div>
+        </header>
+        <div className={styles.chatBox}>
+          <div className={styles.chatHistory}>
+            {chatHistory.map((msg, index) => (
+              <div key={index} className={styles.chatMessage}>
+                {msg}
+              </div>
+            ))}
+          </div>
+          <div className={styles.inputContainer}>
+            <input
+              type="text"
+              value={message}
+              onChange={handleChange}
+              onKeyDown={handleKeyDown}
+              placeholder="Type a message..."
+              className={styles.input}
+            />
+            <button onClick={handleSendMessage} className={styles.sendButton}>
+              <img
+                src="/pointer.png"
+                alt="Arrow"
+                style={{ width: "28px", height: "28px" }}
+              />
+            </button>
+            <button onClick={handleSendVoice} className={styles.voiceButton}>
+              <img
+                src="/miccc.png"
+                alt="Microphone"
+                style={{ width: "26px", height: "26px" }}
+              />
+            </button>
+            <button className={styles.newChatButton}>+</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
